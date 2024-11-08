e5fc8eb5a65a5e0793779b13345b2f1efe530201
Saad Tariq <saadtariq.dev@outlook.com>
2024-11-08 15:00:28 -0500

    feat: Implement initial board and piece structure with utility functions

---
+++ src/frontend/src/features/piece/types.ts
@@ -0,0 +1,15 @@
+export type PieceColour = "black" | "white"
+
+export type PieceType =
+  | "king"
+  | "queen"
+  | "rook"
+  | "bishop"
+  | "knight"
+  | "pawn"
+  | "bPawn"
+
+export type Piece = {
+  type: PieceType
+  colour: PieceColour
+}
---
+++ src/frontend/src/features/piece/utils.ts
@@ -0,0 +1,42 @@
+import type { Board, Cell } from "../board/types"
+
+// TODO: figure out integrating check state to limit moves
+export const getAvailableMoves = (board: Board, cell: Cell): Cell[] => {
+  const cells: Cell[] = []
+
+  if (cell.piece === null) return cells
+
+  switch (cell.piece.type) {
+    case "king":
+      break
+    case "queen":
+      break
+    case "rook":
+      break
+    case "bishop":
+      break
+    case "knight":
+      break
+    case "pawn":
+      break
+    case "bPawn":
+      break
+  }
+
+  return cells
+}
+
+// TODO
+export const movePiece = (board: Board, from: Cell, to: Cell) => {
+  console.info(board, from, to)
+}
+
+// TODO
+export const capturePiece = (board: Board, from: Cell, to: Cell) => {
+  console.info(board, from, to)
+}
+
+// TODO
+export const promotePawn = (board: Board, cell: Cell) => {
+  console.info(board, cell)
+}
