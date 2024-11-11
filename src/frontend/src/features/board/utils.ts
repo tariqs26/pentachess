1efdc4ea9c8516609f99817b5c4d55be67d8c7b3
Saad Tariq <saadtariq.dev@outlook.com>
2024-11-11 16:15:09 -0500

    refactor: enhance initial board generation with cell positioning and adjacency logic, add side chunking function

--- src/frontend/src/features/board/utils.ts
+++ src/frontend/src/features/board/utils.ts
@@ -1,28 +1,48 @@
 import { DECAGON_SIDES, RING_SIZES } from "./constants"
-import type { Board } from "./types"
+import type { Board, Cell } from "./types"
 
-// TODO
+// TODO: Demo MVP
 export const generateInitialBoard = (): Board => {
   const board: Board = []
 
   for (let i = 0; i < RING_SIZES.length; i++) {
     const size = RING_SIZES[i]
-    const ring = []
+    const ring: Cell[] = []
 
     for (let j = 0; j < size; j++) {
-      const cell = {
-        id: `${i === 0 ? "A" : i === 1 ? "B" : "C"}${j + 1}`,
-        colour: j % 2 === 0 ? "white" : "black",
+      const cell: Omit<Cell, "adjacent"> = {
+        id: `${i === 2 ? "A" : i === 1 ? "B" : "C"}${j + 1}`,
+        position: { i, j },
+        colour: (j % 2 === 0 ? "white" : "black") as Cell["colour"],
         side: Math.floor(j / (size / DECAGON_SIDES)) + 1,
         piece: null,
+        vertices: { in: [], out: [] },
       }
 
-      ring.push(cell)
+      ring.push(cell as Cell)
     }
+
+    // assign adjacent cells
+    for (const cell of ring) {
+      cell.adjacent = {
+        prev: ring[(cell.position.j - 1 + size) % size],
+        next: ring[(cell.position.j + 1) % size],
+        in: null,
+        out: null,
+      }
+    }
+
+    board.push(ring)
   }
 
   return board
 }
+
+export const getSides = <T>(arr: T[], size: number) =>
+  arr.reduce((acc: T[][], _, i) => {
+    if (i % size === 0) acc.push(arr.slice(i, i + size))
+    return acc
+  }, [])
 
 // TODO
 export const checkForCheck = (board: Board): boolean => {
