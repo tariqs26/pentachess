d9c3ab604c77b30f8edf165c7381c5eab886de58
Saad Tariq <saadtariq.dev@outlook.com>
2024-11-17 15:22:34 -0500

    refactor: simplify rotation function, move pentagon and piece/id into a container

--- src/frontend/src/features/board/components/Cell.tsx
+++ src/frontend/src/features/board/components/Cell.tsx
@@ -1,41 +1,42 @@
 import Image from "next/image"
 import type { Cell } from "../cell"
 
-const getTransformation = (cell: Omit<Cell, "setVertices">) => {
-  const ring = cell.id.slice(0, 1)
-  const side = cell.side
-  const id = Number(cell.id.slice(1))
-  if (ring != 'A' && side % 2 != 0) {
-    return id % 2 != 0 ? "-70deg" : "-103deg"
+const cellRotation = (cell: Omit<Cell, "setVertices">) => {
+  if (cell.x != 0 && cell.side % 2 != 0) {
+    return cell.y % 2 != 0 ? "-70deg" : "-103deg"
   }
-  return id % 2 == 0 ? "-70deg" : "-103deg"
+  return cell.y % 2 == 0 ? "-70deg" : "-103deg"
 }
 
 export const CellComponent = (cell: Omit<Cell, "setVertices">) => {
   return (
-    <div
-      style={{
-        backgroundColor: cell.color === "w" ? "yellowgreen" : "green",
-        height: "100px",
-        width: "100px",
-        outline: "1px solid black",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        clipPath: "polygon(0% 43.43%,20% 100%,80% 100%,100% 43.43%,50% 76.7%)",
-        rotate: getTransformation(cell),
-        marginLeft: "-70px"
-      }}
-    >
+    <div className="relative">
+      <div
+        style={{
+          backgroundColor: cell.color === "w" ? "yellowgreen" : "green",
+          height: "100px",
+          width: "100px",
+          outline: "1px solid black",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          clipPath:
+            "polygon(0% 43.43%, 20% 100%, 80% 100%, 100% 43.43%, 50% 76.7%)",
+          rotate: cellRotation(cell),
+          marginLeft: "-70px",
+        }}
+      />
       {cell.piece ? (
         <Image
+          className="absolute top-5 size-[50px]"
           src={cell.piece.image}
           alt={`${cell.piece.colour === "w" ? "white" : "black"} ${cell.piece.type}`}
-          style={{ width: "50px", height: "50px" }}
           priority
         />
       ) : (
-        <span style={{ color: "black", fontWeight: "bold" }}>{cell.id}</span>
+        <span className="absolute top-5 font-semibold text-background">
+          {cell.id}
+        </span>
       )}
     </div>
   )
