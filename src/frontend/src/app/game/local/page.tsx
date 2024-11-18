d5e3e6d6128acf96e8a71e3fa96fb26a73397f84
Saad Tariq <saadtariq.dev@outlook.com>
2024-11-18 01:58:16 -0500

    feat: add CapturedPieces component to display captured pieces in LocalGamePage

--- src/frontend/src/app/game/local/page.tsx
+++ src/frontend/src/app/game/local/page.tsx
@@ -1,7 +1,34 @@
 "use client"
 
 import { Board } from "@/features/board/components/Board"
+import { useLocalGame } from "@/features/game/useLocalGame"
+import { Piece } from "@/features/piece/types"
+import Image from "next/image"
+
+const CapturedPieces = ({ pieces }: { pieces: Piece[] }) => (
+  <div className="flex h-[60px] bg-gray-700">
+    {pieces.map((piece, i) => (
+      <Image
+        key={i}
+        src={piece.image}
+        width={50}
+        height={50}
+        alt={piece.type}
+      />
+    ))}
+  </div>
+)
 
 export default function LocalGamePage() {
-  return <Board />
+  const { state } = useLocalGame()
+
+  return (
+    <main className="grid min-h-screen place-items-center">
+      <div className="container bg-gray-800">
+        <CapturedPieces pieces={state.capturedPieces["w"]} />
+        <Board />
+        <CapturedPieces pieces={state.capturedPieces["b"]} />
+      </div>
+    </main>
+  )
 }
