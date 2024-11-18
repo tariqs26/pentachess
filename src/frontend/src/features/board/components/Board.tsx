50668441421c2aa74fa66bf8f5beef2c78e91795
Saad Tariq <saadtariq.dev@outlook.com>
2024-11-18 01:57:57 -0500

    fix: adjust Board component styling and address TypeScript error in CellComponent

--- src/frontend/src/features/board/components/Board.tsx
+++ src/frontend/src/features/board/components/Board.tsx
@@ -8,12 +8,13 @@ export const Board = () => {
   const { state } = useLocalGame()
 
   return (
-    <div className="relative m-auto size-[800px] bg-gray-800">
+    <div className="relative mx-auto size-[650px]">
       {state.boardState.board.map((ring, i) => (
         <div key={i} className="absolute left-1/2 top-1/2">
           {getSides(ring, ring.length / DECAGON_SIDES).map((side, j) => (
             <Side key={j} ring={i} side={j}>
               {side.map((cell) => (
+                // @ts-expect-error: issue with destructuring function from class object
                 <CellComponent key={cell.id} {...cell} />
               ))}
             </Side>
