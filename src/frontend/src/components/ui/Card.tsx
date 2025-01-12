48555036f02a0468c38fa91a1e58eadc5cbfe910
Saad Tariq <saadtariq.dev@outlook.com>
2025-01-12 18:57:38 -0500

    feat: add Card component

---
+++ src/frontend/src/components/ui/Card.tsx
@@ -0,0 +1,56 @@
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+
+const Card = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) => (
+  <div
+    className={cn(
+      "rounded-xl border bg-card text-card-foreground shadow",
+      className
+    )}
+    {...props}
+  />
+)
+
+const CardHeader = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) => (
+  <div className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />
+)
+
+const CardTitle = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) => (
+  <div
+    className={cn("font-semibold leading-none tracking-tight", className)}
+    {...props}
+  />
+)
+
+const CardDescription = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) => (
+  <div className={cn("text-sm text-muted-foreground", className)} {...props} />
+)
+
+const CardContent = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) => (
+  <div className={cn("p-6 pt-0", className)} {...props} />
+)
+
+const CardFooter = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) => (
+  <div className={cn("flex items-center p-6 pt-0", className)} {...props} />
+)
+
+export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
