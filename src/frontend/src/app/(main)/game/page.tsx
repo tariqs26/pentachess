a9cf26e0f46b97b012a086a6fb2894c7c911fe20
Saad Tariq <saadtariq.dev@outlook.com>
2025-01-23 03:22:28 -0500

    feat: add game creation and joining forms

--- src/frontend/src/app/(main)/game/page.tsx
+++ src/frontend/src/app/(main)/game/page.tsx
@@ -1,12 +1,27 @@
-// TODO: Implement GamePage
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/Card"
+import { CreateGameForm } from "@/features/game/components/CreateGameForm"
+import { JoinGameForm } from "@/features/game/components/JoinGameForm"
+
 export default function GamePage() {
   return (
-    <>
-      <h2>Create Game</h2>
-      <form></form>
-      <hr />
-      <h2>Join Game</h2>
-      <form></form>
-    </>
+    <div className="mx-auto grid min-h-screen max-w-5xl place-items-center p-6">
+      <Card>
+        <CardHeader className="pb-3">
+          <CardTitle className="text-xl">Create Game</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <CreateGameForm isOnline />
+        </CardContent>
+        <div className="flex items-center gap-4 text-sm font-medium text-muted-foreground">
+          <hr className="flex-grow" /> OR <hr className="flex-grow" />
+        </div>
+        <CardHeader className="py-3">
+          <CardTitle className="text-xl">Join Game</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <JoinGameForm />
+        </CardContent>
+      </Card>
+    </div>
   )
 }
