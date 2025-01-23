7249381c2ef0f0028a3aa390fb2f5a2d3826a71c
Saad Tariq <saadtariq.dev@outlook.com>
2025-01-23 03:23:45 -0500

    style: update layout of HomePage with grid and flexbox

--- src/frontend/src/app/(main)/page.tsx
+++ src/frontend/src/app/(main)/page.tsx
@@ -1,17 +1,17 @@
 import Link from "next/link"
 import { Button } from "@/components/ui/Button"
 
-// TODO: Implement HomePage
 export default function HomePage() {
   return (
-    <>
-      <Button asChild>
-        <Link href="/game">Create/Join Game</Link>
-      </Button>
-
-      <Button asChild variant="secondary">
-        <Link href="/game/local">Local Game</Link>
-      </Button>
-    </>
+    <div className="grid min-h-screen place-items-center p-6">
+      <div className="flex gap-2">
+        <Button asChild>
+          <Link href="/game">Create/Join Game</Link>
+        </Button>
+        <Button asChild variant="secondary">
+          <Link href="/game/local">Local Game</Link>
+        </Button>
+      </div>
+    </div>
   )
 }
