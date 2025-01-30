2de230cafb84ec8636a2a92a3c33c3c7ba6aa97b
Saad Tariq <saadtariq.dev@outlook.com>
2025-01-30 15:45:12 -0500

    fix: update cell piece positions

--- src/frontend/src/features/board/components/Cell.tsx
+++ src/frontend/src/features/board/components/Cell.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image"
 import { useLocalGame } from "@/features/game/useLocalGame"
+import { cn } from "@/lib/utils"
 import type { Cell } from "../types"
-import { sideRotation, sideLeft, sideTop } from "./Side"
+import { sideRotation } from "./Side"
 
 const cellRotation = (cell: Cell) => {
   if (cell.x == 0) {
@@ -15,59 +16,67 @@ const cellRotation = (cell: Cell) => {
 
 const marginLeftStyle = (cell: Cell) => {
   // key = the ith cell in the side, value = margin value
-  const leftMarginsForRing1 = {0: 0, 1: -69, 2: -69.5}
-  const leftMarginsForRing2 = {0: 0, 1: -69, 2: -69.5, 3: -69, 4: -69.5}
+  /** TODO: ensure margins are similar in value for all cells
+   * the first cells in each side have margin 0 which is causing
+   * issues with overlapping as the first div in each side takes up too much space
+   * temp fix is update z-index in side but it causes at least
+   * one side in the outer and middle ring to be inaccessible
+   * to fix this we will need to update all side positions for
+   * the outer and middle ring after fixing the margins
+   */
+  const leftMarginsForRing1 = { 0: 0, 1: -69, 2: -69.5 }
+  const leftMarginsForRing2 = { 0: 0, 1: -69, 2: -69.5, 3: -69, 4: -69.5 }
 
   if (cell.x == 1) {
-    const ithCell = (cell.y - (cell.side * 3)) as keyof typeof leftMarginsForRing1
-    return leftMarginsForRing1[ithCell];
+    const ithCell = (cell.y - cell.side * 3) as keyof typeof leftMarginsForRing1
+    return leftMarginsForRing1[ithCell]
   }
 
   if (cell.x == 2) {
-    const ithCell = (cell.y - (cell.side * 5)) as keyof typeof leftMarginsForRing2
-    return leftMarginsForRing2[ithCell];
+    const ithCell = (cell.y - cell.side * 5) as keyof typeof leftMarginsForRing2
+    return leftMarginsForRing2[ithCell]
   }
-  // Default margins for Ring 0
+  // default margins for ring 0
   return cell.side % 2 != 0 ? -70.1 : -70.5
 }
 
 const marginTopStyle = (cell: Cell) => {
   // key = the ith cell in the side, value = margin value
-  const topMarginsForRing1 = {0: 0, 1: 4.5, 2: -1.5}
-  const topMarginsForRing2 = {0: 0, 1: 4.5, 2: -1.6, 3: 2.9, 4: -3.2}
+  // TODO: ensure margins are similar in value for all cells
+  const topMarginsForRing1 = { 0: 0, 1: 4.5, 2: -1.5 }
+  const topMarginsForRing2 = { 0: 0, 1: 4.5, 2: -1.6, 3: 2.9, 4: -3.2 }
 
   if (cell.x == 1) {
-    const ithCell = (cell.y - (cell.side * 3)) as keyof typeof topMarginsForRing1
-    return topMarginsForRing1[ithCell];
+    const ithCell = (cell.y - cell.side * 3) as keyof typeof topMarginsForRing1
+    return topMarginsForRing1[ithCell]
   }
   if (cell.x == 2) {
-    const ithCell = (cell.y - (cell.side * 5)) as keyof typeof topMarginsForRing2
-    return topMarginsForRing2[ithCell];
+    const ithCell = (cell.y - cell.side * 5) as keyof typeof topMarginsForRing2
+    return topMarginsForRing2[ithCell]
   }
 
-  // Default margins for Ring 0
+  // default margins for ring 0
   return cell.side % 2 != 0 ? -30 : -10
 }
 
 const pieceRotation = (cell: Cell) =>
-  `calc(${-cellRotation(cell) - sideRotation(cell.x)[cell.side] }deg )`
-
-const pieceLeft = (cell: Cell) =>
-  `${-marginLeftStyle(cell)}px`
-
-const pieceTop = (cell: Cell) =>
-  `${-marginTopStyle(cell)}px`
+  `calc(${-cellRotation(cell) - sideRotation[cell.x][cell.side]}deg )`
 
 export const CellComponent = (cell: Cell) => {
   const { state, dispatch } = useLocalGame()
 
   const isCellSelected = state.boardState.selectedCell?.cell.id === cell.id
-  const isAvailableMove = state.boardState.selectedCell?.availableMoves.values().some((move) => move.id === cell.id)
+  const isAvailableMove = state.boardState.selectedCell?.availableMoves
+    .values()
+    .some((move) => move.id === cell.id)
 
   const handlePieceMouseDown = () => {
     if (state.boardState.disabled) return
     if (cell.piece === null || cell.piece.color !== state.turn) return
-    if (isCellSelected) return
+    if (isCellSelected) {
+      dispatch({ type: "SELECT_CELL", payload: null })
+      return
+    }
     dispatch({ type: "SELECT_CELL", payload: cell })
   }
 
@@ -79,20 +88,16 @@ export const CellComponent = (cell: Cell) => {
     }
     if (!state.boardState.selectedCell.cell.piece) return
     if (state.boardState.overCell === null) return
+
+    const { x, y, piece } = state.boardState.selectedCell.cell
+    const { x: toX, y: toY, piece: toPiece } = state.boardState.overCell
+
     dispatch({
       type: "MOVE_PIECE",
       payload: {
-        from: {
-          x: state.boardState.selectedCell.cell.x,
-          y: state.boardState.selectedCell.cell.y,
-          piece: state.boardState.selectedCell.cell.piece,
-        },
-        to: {
-          x: state.boardState.overCell.x,
-          y: state.boardState.overCell.y,
-          piece: state.boardState.overCell.piece,
-        },
-        piece: state.boardState.selectedCell.cell.piece,
+        from: { x, y, piece },
+        to: { x: toX, y: toY, piece: toPiece },
+        piece,
       },
     })
   }
@@ -112,6 +117,7 @@ export const CellComponent = (cell: Cell) => {
 
   return (
     <div
+      id={`cell-container-${cell.id}`}
       className="relative"
       onMouseUp={handlePieceMouseUp}
       onMouseEnter={handleCellMouseEnter}
@@ -120,62 +126,42 @@ export const CellComponent = (cell: Cell) => {
       onDragEnd={handlePieceMouseUp}
     >
       <div
-        style={{
-          backgroundColor: isAvailableMove
-            ? "green"
+        id={`cell-${cell.id}`}
+        className={cn(
+          "flex size-[100px] items-center justify-center",
+          isAvailableMove && "hover:cursor-pointer",
+          isAvailableMove
+            ? cell.piece
+              ? "bg-red-500"
+              : "bg-green-500"
             : isCellSelected
-              ? "red"
+              ? "bg-orange-500"
               : cell.color === "w"
-                ? "white"
-                : "gray",
-
-          height: "100px",
-          width: "100px",
-          outline: "1px solid black",
-          display: "flex",
-          position: "relative",
-          justifyContent: "center",
-          alignItems: "center",
-          clipPath: "polygon(0% 41.2215%, 19.0983% 100%, 80.9017% 100%, 100% 41.2215%, 50% 77.5486%)",
+                ? "bg-white"
+                : "bg-gray-500"
+        )}
+        style={{
+          clipPath:
+            "polygon(0% 41.2215%, 19.0983% 100%, 80.9017% 100%, 100% 41.2215%, 50% 77.5486%)",
           rotate: `${cellRotation(cell)}deg`,
           marginLeft: `${marginLeftStyle(cell)}px`,
           marginTop: `${marginTopStyle(cell)}px`,
-          zIndex: cell.color === "b" ? 2 : 1
         }}
       >
-        { cell.piece && (
+        {cell.piece && (
           <Image
-          src={cell.piece.image}
-          alt={`${cell.piece.color === "w" ? "white" : "black"} ${cell.piece.type}`}
-          draggable
-          priority
-          onMouseDown={handlePieceMouseDown}
-          style={{
-            position: "absolute",
-            marginLeft: pieceLeft(cell),
-            marginTop: pieceTop(cell),
-            transform: "translate(-50%, -50%)",
-            width: "25px",
-            height: "25px",
-            rotate: pieceRotation(cell),
-            zIndex: 4,
-            cursor: "pointer"
-          }}
-        />
+            src={cell.piece.image}
+            alt={`${cell.piece.color === "w" ? "white" : "black"} ${cell.piece.type}`}
+            className="absolute left-[50%] top-[70%] z-[999] size-[30px] hover:cursor-pointer"
+            style={{ rotate: pieceRotation(cell) }}
+            draggable
+            priority
+            onMouseDown={handlePieceMouseDown}
+          />
         )}
         <span
-          style={{
-            position: "absolute",
-            left: "23%",
-            top: "77%",
-            transform: "translate(-50%, -50%)",
-            fontSize: "8px",
-            fontWeight: "bold",
-            color: "black",
-            rotate: pieceRotation(cell),
-            pointerEvents: "none",
-            zIndex: 2,
-          }}
+          className="pointer-events-none absolute left-[20%] top-[80%] select-none text-[10px] font-bold text-black"
+          style={{ rotate: pieceRotation(cell) }}
         >
           {cell.id}
         </span>
--- src/frontend/src/features/board/components/Side.tsx
+++ src/frontend/src/features/board/components/Side.tsx
@@ -1,52 +1,42 @@
-import type { CSSProperties } from "react"
+export const sideRotation = [
+  // ring 0
+  [-20, -17.5, -92, -89.5, -164, -161.5, 124, 126.5, 52, 54.6],
+  // ring 1
+  [19, -17, -53, -89, -125, -161, 163, 127, 91, 55],
+  // ring 2
+  [19, -17, -53, -89, -125, -161, 163, 127, 91, 55],
+]
 
-type SideProps = React.PropsWithChildren<{
-  ring: number
-  side: number
-}>
+export const sideLeft = [
+  // ring 0
+  [56.57, 87, 66.5, 72.2, 2, -25.2, -48.0, -70.8, -14.3, -1.4],
+  // ring 1
+  [-74, 14, 76.2, 89, 47.3, -32.7, -120.6, -182.8, -195.5, -153.8],
+  // ring 2
+  [-133.5, 13.5, 123, 153.5, 93, -35, -181.9, -291.2, -321.8, -261.3],
+]
 
-export const sideRotation = (ring: number) => {
-  switch (ring) {
-    case 0:
-      return [-20, -17.5, -92, -89.5, -164, -161.5, 124, 126.5, 52, 54.6]
-    case 1:
-      return [19, -17, -53, -89, -125, -161, 163, 127, 91, 55]
-    default:
-      return [19, -17, -53, -89, -125, -161, 163, 127, 91, 55]
-  }
-}
+export const sideTop = [
+  // ring 0
+  [0, 6.2, -71, -91.3, -102.5, -107.5, -50.9, -19.8, 12.5, 50.5],
+  // ring 1
+  [93.5, 78.2, 14.2, -74.1, -153.1, -192.5, -177.3, -113.5, -25.1, 54],
+  // ring 2
+  [184.5, 168.7, 69.5, -75, -210, -283.5, -267.8, -169, -24.5, 110.5],
+]
 
-export const sideLeft = (ring: number) => {
-  switch (ring) {
-    case 0:
-      return [56.57, 87, 66.5, 72.2, 2, -25.2, -48.0, -70.8, -14.3, -1.4]
-    case 1:
-      return [-74, 14, 76.2, 89, 47.3, -32.7, -120.6, -182.8, -195.5, -153.8]
-    default:
-      return [-133.5, 13.5, 123, 153.5, 93, -35, -181.9, -291.2, -321.8, -261.3]
-  }
-}
+type SideProps = React.PropsWithChildren<{ ring: number; side: number }>
 
-export const sideTop = (ring: number) => {
-  switch (ring) {
-    case 0:
-      return [0, 6.2, -71, -91.3, -102.5, -107.5, -50.9, -19.8, 12.5, 50.5]
-    case 1:
-      return [93.5, 78.2, 14.2, -74.1, -153.1, -192.5, -177.3, -113.5, -25.1, 54]
-    default:
-      return [184.5, 168.7, 69.5, -75, -210, -283.5, -267.8, -169, -24.5, 110.5]
-  }
-}
-
-const sideStyle = (ring: number, side: number): CSSProperties => ({
-  display: "flex",
-  position: "absolute",
-  rotate: `${sideRotation(ring)[side]}deg`,
-  top: `${sideTop(ring)[side]}px`,
-  left: `${sideLeft(ring)[side]}px`,
-  zIndex: side % 2 == 0 ? 2 : 1,
-})
-
-export const Side = ({ side, ring, ...props }: SideProps) => {
-  return <div style={sideStyle(ring, side)} {...props} />
-}
+export const Side = ({ side, ring, ...props }: SideProps) => (
+  <div
+    id={`side-${ring}-${side}`}
+    className="absolute flex"
+    style={{
+      rotate: `${sideRotation[ring][side]}deg`,
+      left: `${sideLeft[ring][side]}px`,
+      top: `${sideTop[ring][side]}px`,
+      zIndex: 10 - side, // TODO: temp fix for overlapping sides (see Cell.tsx): when issue with cell margins is fixed, remove this
+    }}
+    {...props}
+  />
+)
