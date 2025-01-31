510a2b9f74de77b3e499640897a4634d681eefc2
khanm345 <khanm345@mcmaster.ca>
2025-01-30 22:10:38 -0500

    fix: piece and label positioning (#45)
    * fix: piece and label positioning
    
    ---------
    
    Co-authored-by: Saad Tariq <saadtariq.dev@outlook.com>

--- src/frontend/src/features/board/components/Cell.tsx
+++ src/frontend/src/features/board/components/Cell.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import { useLocalGame } from "@/features/game/useLocalGame"
+import { cn } from "@/lib/utils"
 import type { Cell } from "../types"
 import { sideRotation } from "./Side"
 
@@ -15,53 +16,58 @@ const cellRotation = (cell: Cell) => {
 
 const marginLeftStyle = (cell: Cell) => {
   // key = the ith cell in the side, value = margin value
-  const leftMarginsForRing1 = {0: 0, 1: -69, 2: -69.5}
-  const leftMarginsForRing2 = {0: 0, 1: -69, 2: -69.5, 3: -69, 4: -69.5}
+  const leftMarginsForRing1 = { 0: -70, 1: -69, 2: -69.5 }
+  const leftMarginsForRing2 = { 0: -70, 1: -69, 2: -69.5, 3: -69, 4: -69.5 }
 
   if (cell.x == 1) {
-    const ithCell = (cell.y - (cell.side * 3)) as keyof typeof leftMarginsForRing1
-    return leftMarginsForRing1[ithCell];
+    const ithCell = (cell.y - cell.side * 3) as keyof typeof leftMarginsForRing1
+    return leftMarginsForRing1[ithCell]
   }
 
   if (cell.x == 2) {
-    const ithCell = (cell.y - (cell.side * 5)) as keyof typeof leftMarginsForRing2
-    return leftMarginsForRing2[ithCell];
+    const ithCell = (cell.y - cell.side * 5) as keyof typeof leftMarginsForRing2
+    return leftMarginsForRing2[ithCell]
   }
-  // Default margins for Ring 0
+  // default margins for ring 0
   return cell.side % 2 != 0 ? -70.1 : -70.5
 }
 
 const marginTopStyle = (cell: Cell) => {
   // key = the ith cell in the side, value = margin value
-  const topMarginsForRing1 = {0: 0, 1: 4.5, 2: -1.5}
-  const topMarginsForRing2 = {0: 0, 1: 4.5, 2: -1.6, 3: 2.9, 4: -3.2}
+  const topMarginsForRing1 = { 0: 0, 1: 4.5, 2: -1.5 }
+  const topMarginsForRing2 = { 0: 0, 1: 4.5, 2: -1.6, 3: 2.9, 4: -3.2 }
 
   if (cell.x == 1) {
-    const ithCell = (cell.y - (cell.side * 3)) as keyof typeof topMarginsForRing1
-    return topMarginsForRing1[ithCell];
+    const ithCell = (cell.y - cell.side * 3) as keyof typeof topMarginsForRing1
+    return topMarginsForRing1[ithCell]
   }
   if (cell.x == 2) {
-    const ithCell = (cell.y - (cell.side * 5)) as keyof typeof topMarginsForRing2
-    return topMarginsForRing2[ithCell];
+    const ithCell = (cell.y - cell.side * 5) as keyof typeof topMarginsForRing2
+    return topMarginsForRing2[ithCell]
   }
 
-  // Default margins for Ring 0
+  // default margins for ring 0
   return cell.side % 2 != 0 ? -30 : -10
 }
 
 const pieceRotation = (cell: Cell) =>
-  `calc(${-sideRotation(cell.x)[cell.side]}deg )`
+  `calc(${-cellRotation(cell) - sideRotation[cell.x][cell.side]}deg )`
 
 export const CellComponent = (cell: Cell) => {
   const { state, dispatch } = useLocalGame()
 
   const isCellSelected = state.boardState.selectedCell?.cell.id === cell.id
-  const isAvailableMove = state.boardState.selectedCell?.availableMoves.values().some((move) => move.id === cell.id)
+  const isAvailableMove = state.boardState.selectedCell?.availableMoves
+    .values()
+    .some((move) => move.id === cell.id)
 
   const handlePieceMouseDown = () => {
     if (state.boardState.disabled) return
     if (cell.piece === null || cell.piece.color !== state.turn) return
-    if (isCellSelected) return
+    if (isCellSelected) {
+      dispatch({ type: "SELECT_CELL", payload: null })
+      return
+    }
     dispatch({ type: "SELECT_CELL", payload: cell })
   }
 
@@ -73,20 +79,16 @@ export const CellComponent = (cell: Cell) => {
     }
     if (!state.boardState.selectedCell.cell.piece) return
     if (state.boardState.overCell === null) return
+
+    const { x, y, piece } = state.boardState.selectedCell.cell
+    const { x: toX, y: toY, piece: toPiece } = state.boardState.overCell
+
     dispatch({
       type: "MOVE_PIECE",
       payload: {
-        from: {
-          x: state.boardState.selectedCell.cell.x,
-          y: state.boardState.selectedCell.cell.y,
-          piece: state.boardState.selectedCell.cell.piece,
-        },
-        to: {
-          x: state.boardState.overCell.x,
-          y: state.boardState.overCell.y,
-          piece: state.boardState.overCell.piece,
-        },
-        piece: state.boardState.selectedCell.cell.piece,
+        from: { x, y, piece },
+        to: { x: toX, y: toY, piece: toPiece },
+        piece,
       },
     })
   }
@@ -106,6 +108,7 @@ export const CellComponent = (cell: Cell) => {
 
   return (
     <div
+      id={`cell-container-${cell.id}`}
       className="relative"
       onMouseUp={handlePieceMouseUp}
       onMouseEnter={handleCellMouseEnter}
@@ -114,47 +117,46 @@ export const CellComponent = (cell: Cell) => {
       onDragEnd={handlePieceMouseUp}
     >
       <div
-        style={{
-          backgroundColor: isAvailableMove
-            ? "green"
+        id={`cell-${cell.id}`}
+        className={cn(
+          "flex size-[100px] items-center justify-center",
+          isAvailableMove && "hover:cursor-pointer",
+          isAvailableMove
+            ? cell.piece
+              ? "bg-red-500"
+              : "bg-green-500"
             : isCellSelected
-              ? "red"
+              ? "bg-orange-500"
               : cell.color === "w"
-                ? "white"
-                : "gray",
-
-          height: "100px",
-          width: "100px",
-          outline: "1px solid black",
-          display: "flex",
-          position: "relative",
-          justifyContent: "center",
-          alignItems: "center",
-          clipPath: "polygon(0% 41.2215%, 19.0983% 100%, 80.9017% 100%, 100% 41.2215%, 50% 77.5486%)",
+                ? "bg-white"
+                : "bg-gray-500"
+        )}
+        style={{
+          clipPath:
+            "polygon(0% 41.2215%, 19.0983% 100%, 80.9017% 100%, 100% 41.2215%, 50% 77.5486%)",
           rotate: `${cellRotation(cell)}deg`,
           marginLeft: `${marginLeftStyle(cell)}px`,
           marginTop: `${marginTopStyle(cell)}px`,
-          zIndex: cell.color === "b" ? 2 : 1
         }}
-      />
-    {cell.piece ? (
-        <Image
-          src={cell.piece.image}
-          alt={`${cell.piece.color === "w" ? "white" : "black"} ${cell.piece.type}`}
-          className="absolute top-8 z-[999] size-[30px] hover:cursor-pointer"
-          style={{ rotate: pieceRotation(cell) }}
-          priority
-          draggable
-          onMouseDown={handlePieceMouseDown}
-        />
-      ) : (
+      >
+        {cell.piece && (
+          <Image
+            src={cell.piece.image}
+            alt={`${cell.piece.color === "w" ? "white" : "black"} ${cell.piece.type}`}
+            className="absolute left-[50%] top-[70%] z-[999] size-[30px] hover:cursor-pointer"
+            style={{ rotate: pieceRotation(cell) }}
+            draggable
+            priority
+            onMouseDown={handlePieceMouseDown}
+          />
+        )}
         <span
-          className="absolute top-5 select-none font-semibold text-background"
+          className="pointer-events-none absolute left-[20%] top-[80%] select-none text-[10px] font-bold text-black"
           style={{ rotate: pieceRotation(cell) }}
         >
           {cell.id}
         </span>
-      )}
+      </div>
     </div>
   )
 }
