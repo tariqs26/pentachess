ec290f29ec28dc01c43b8d6e741d81cc36b7f1d8
Saad Tariq <saadtariq.dev@outlook.com>
2025-02-01 18:27:40 -0500

    refactor: simplify pawn promotion UI and logic, using constants for promotion pieces and improving styling

--- src/frontend/src/features/piece/components/PromoteIcon.tsx
+++ src/frontend/src/features/piece/components/PromoteIcon.tsx
@@ -1,77 +1,40 @@
+import Image from "next/image"
 import { useLocalGame } from "@/features/game/useLocalGame"
 import { Piece } from "@/features/piece/types"
 import { makePiece } from "@/features/piece/utils"
-import Image from "next/image"
+import { PROMOTION_PIECES } from "../constants"
+import { cn } from "@/lib/utils"
 
 export const PromoteIcon = () => {
   const { state, dispatch } = useLocalGame()
 
-  function onPromote(piece: Piece) {
-    console.log("Pawn Promotion")
-    dispatch({
-      type: "PROMOTE_PAWN",
-      payload: {
-        cell: state.promoteID,
-        piece: piece,
-      },
-    })
+  function handlePawnPromotion(piece: Piece) {
+    dispatch({ type: "PROMOTE_PAWN", payload: piece })
   }
 
+  const pieceColor = state.turn === "w" ? "b" : "w"
+
   return (
-    <div
-      style={{
-        position: "absolute",
-        top: 0,
-        left: 0,
-        width: "100%",
-        height: "100%",
-        background: "rgba(0, 0, 0, 0.5)",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        zIndex: 50,
-      }}
-    >
-      <div
-        id="promotePopup"
-        style={{
-          position: "relative",
-          background: "rgba(90, 125, 238, 0.2)",
-          padding: "20px",
-          borderRadius: "8px",
-          boxShadow: "0 4px 15px rgba(0, 0, 0, 0.2)",
-          textAlign: "center",
-          zIndex: 100,
-        }}
-      >
-        <h2 style={{ marginBottom: "20px" }}>Promote Your Pawn</h2>
-        <div style={{ display: "flex", justifyContent: "center", gap: "10px" }}>
-          {[
-            makePiece("queen", state.turn === "w" ? "b" : "w"),
-            makePiece("rook", state.turn === "w" ? "b" : "w"),
-            makePiece("bishop", state.turn === "w" ? "b" : "w"),
-            makePiece("knight", state.turn === "w" ? "b" : "w"),
-          ].map((piece) => (
-            <button
-              key={piece.type}
-              onClick={() => onPromote(piece)}
-              style={{
-                padding: "10px",
-                fontSize: "16px",
-                background: "rgba(0, 0, 0, 0.2)",
-                border: "1px solid #ccc",
-                borderRadius: "5px",
-                cursor: "pointer",
-              }}
-            >
-              <Image
+    <div className="absolute inset-0 grid place-items-center bg-black/50">
+      <div className="relative rounded-xl border bg-card p-4 text-center shadow-lg">
+        <h2 className="mb-4 font-semibold leading-none tracking-tight">
+          Promote Your Pawn
+        </h2>
+        <div className="flex gap-3">
+          {PROMOTION_PIECES.map((piece) => makePiece(piece, pieceColor)).map(
+            (piece) => (
+              <button
                 key={piece.type}
-                src={piece.image}
-                alt={piece.type}
-                className="size-10"
-              />
-            </button>
-          ))}
+                onClick={() => handlePawnPromotion(piece)}
+                className={cn(
+                  "rounded-md border bg-secondary p-2.5 text-secondary-foreground shadow-sm transition-colors hover:cursor-pointer hover:bg-secondary/80 hover:text-accent-foreground",
+                  pieceColor === "b" && "dark:brightness-110 dark:filter"
+                )}
+              >
+                <Image src={piece.image} alt={piece.type} className="size-10" />
+              </button>
+            )
+          )}
         </div>
       </div>
     </div>
