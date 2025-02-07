e02c8300af1e26b77d68f2836626f75d892bdf08
Saad Tariq <saadtariq.dev@outlook.com>
2025-02-06 21:15:19 -0500

    fix: cloning logic for cell edges and vertices

--- src/frontend/src/features/board/cell.ts
+++ src/frontend/src/features/board/cell.ts
@@ -155,5 +155,9 @@ export function setCellVertices({ x, y, edges, vertices }: Cell, board: Board) {
 }
 
 export function cloneCell(cell: Cell): Cell {
-  return { ...cell, edges: { ...cell.edges }, vertices: [...cell.vertices] }
+  return {
+    ...cell,
+    edges: cell.edges.map((edge) => [...edge]),
+    vertices: cell.vertices.map((vertex) => [...vertex]),
+  }
 }
