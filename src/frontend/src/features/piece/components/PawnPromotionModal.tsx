b4c0f7fd8ca7a0f67c7c77f7b5529f9a2af273fc
Saad Tariq <saadtariq.dev@outlook.com>
2025-02-25 13:58:51 -0500

    refactor: decouple local game state from `PawnPromotionModal` (#67)
    * refactor: decouple local game state from `PawnPromotionModal` by passing `turn` and `handlePromotion` as props
    
    * refactor: update imports in `PawnPromotionModal` to use relative paths

--- src/frontend/src/features/piece/components/PawnPromotionModal.tsx
+++ src/frontend/src/features/piece/components/PawnPromotionModal.tsx
@@ -1,49 +1,45 @@
 import Image from "next/image"
-import { useLocalGame } from "@/features/game/useLocalGame"
-import type { Piece } from "@/features/piece/types"
-import { makePiece } from "@/features/piece/utils"
 import { cn } from "@/lib/utils"
 import { PROMOTION_PIECES } from "../constants"
+import type { Piece, PieceColor } from "../types"
+import { makePiece } from "../utils"
 
-export const PawnPromotionModal = () => {
-  const { state, dispatch } = useLocalGame()
+type PawnPromotionModalProps = Readonly<{
+  turn: PieceColor
+  handlePromotion: (piece: Piece) => void
+}>
 
-  function handlePawnPromotion(piece: Piece) {
-    dispatch({ type: "PROMOTE_PAWN", payload: piece })
-  }
-
-  return (
-    <>
-      <div className="absolute inset-0 z-10 bg-black/50" />
-      <div className="absolute inset-0 z-10 flex items-center justify-start">
-        <div className="ml-4 rounded-xl border bg-card p-4 text-center shadow-lg">
-          <h2 className="mb-4 font-semibold leading-none tracking-tight">
-            Promote
-            <br />
-            Pawn
-          </h2>
-          <div className="flex flex-col gap-3">
-            {PROMOTION_PIECES.map((piece) => makePiece(piece, state.turn)).map(
-              (piece) => (
-                <button
-                  key={piece.type}
-                  onClick={() => handlePawnPromotion(piece)}
-                  className={cn(
-                    "rounded-md border bg-secondary p-2.5 text-secondary-foreground shadow-sm transition-colors hover:cursor-pointer hover:bg-secondary/80 hover:text-accent-foreground",
-                    state.turn === "b" && "dark:brightness-110 dark:filter"
-                  )}
-                >
-                  <Image
-                    src={piece.image}
-                    alt={piece.type}
-                    className="size-10"
-                  />
-                </button>
-              )
-            )}
-          </div>
+export const PawnPromotionModal = ({
+  turn,
+  handlePromotion,
+}: PawnPromotionModalProps) => (
+  <>
+    <div className="absolute inset-0 z-10 bg-black/50" />
+    <div className="absolute inset-0 z-10 flex items-center justify-start">
+      <div className="ml-4 rounded-xl border bg-card p-4 text-center shadow-lg">
+        <h2 className="mb-4 font-semibold leading-none tracking-tight">
+          Promote
+          <br />
+          Pawn
+        </h2>
+        <div className="flex flex-col gap-3">
+          {PROMOTION_PIECES.map((piece) => makePiece(piece, turn)).map(
+            (piece) => (
+              <button
+                key={piece.type}
+                type="button"
+                className={cn(
+                  "rounded-md border bg-secondary p-2.5 text-secondary-foreground shadow-sm transition-colors hover:cursor-pointer hover:bg-secondary/80 hover:text-accent-foreground",
+                  turn === "b" && "dark:brightness-110 dark:filter"
+                )}
+                onClick={() => handlePromotion(piece)}
+              >
+                <Image src={piece.image} alt={piece.type} className="size-10" />
+              </button>
+            )
+          )}
         </div>
       </div>
-    </>
-  )
-}
+    </div>
+  </>
+)
