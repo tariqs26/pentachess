6053eea8c10f8ca4944a15778be77b603f0975da
Saad Tariq <saadtariq.dev@outlook.com>
2025-03-19 20:57:48 -0400

    refactor: improve socket connection handling and add delay before joining game

--- src/frontend/src/app/(main)/game/online/useGame.ts
+++ src/frontend/src/app/(main)/game/online/useGame.ts
@@ -3,92 +3,77 @@ import { useLocalGame } from "@/features/game/useLocalGame"
 import { isGameOver } from "@/features/game/utils"
 import type { Piece } from "@/features/piece/types"
 import { type ServerToClientEvents, socket } from "@/lib/socket"
+import { sleep } from "@/lib/utils"
 
 export function useGame(userId: string, username: string) {
   const { state, dispatch } = useLocalGame()
   const [connected, setConnected] = useState(false)
 
-  // Effect: Socket events
+  // effect: socket connection/disconnection
   useEffect(() => {
-    const onConnect = () => {
-      // Prevent auto-reconnect after game ends
-      if (state.status !== "waiting") return
-      console.info("Connected as:", { userId, username })
+    socket.connect()
+
+    const onConnect = async () => {
       setConnected(true)
+      await sleep(1000)
       socket.emit("join", { userId, username })
     }
 
-    if (socket.connected) onConnect()
-
     const onDisconnect = () => {
-      console.info("Disconnected")
       setConnected(false)
     }
 
-    if (socket.disconnected) setConnected(false)
+    socket.on("connect", onConnect)
+    socket.on("disconnect", onDisconnect)
 
+    return () => {
+      socket.off("connect", onConnect)
+      socket.off("disconnect", onDisconnect)
+      socket.disconnect()
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  // effect: socket events
+  useEffect(() => {
     const onStartGame: ServerToClientEvents["start"] = (data) => {
-      console.info("Game started:", data)
-      const players = data.players
-      if (players[0].userId !== userId) {
-        // eslint-disable-next-line no-extra-semi
-        ;[players[0], players[1]] = [players[1], players[0]]
-      }
+      if (data.players[0].id !== socket.id) data.players.reverse()
       dispatch({ type: "RESET_GAME" })
-      dispatch({ type: "START_GAME", players })
+      dispatch({ type: "START_GAME", players: data.players })
     }
 
     const onMove: ServerToClientEvents["move"] = (newState) => {
       if (newState.turn !== state.player.color) return
-      console.info("📥 Received move", newState)
       dispatch({ type: "SYNC_GAME", state: newState })
     }
 
-    const onLeave = () => {
-      if (state.status === "playing") {
-        console.info("Opponent left the game")
-        dispatch({ type: "SET_WINNER", player: state.player.color })
-        dispatch({ type: "SET_STATUS", status: "opponent-left" })
-      } else {
-        // TODO: Might not be necessary
-        console.info("Rejoining queue as:", { userId, username })
-        dispatch({ type: "RESET_GAME" })
-        socket.emit("join", { userId, username })
-      }
+    const onLeave: ServerToClientEvents["leave"] = () => {
+      if (state.status !== "playing") return
+      dispatch({ type: "SET_STATUS", status: "opponent-left" })
     }
 
     const onGameEnd: ServerToClientEvents["end"] = (data) => {
-      console.info("📥 Received game end:", data, state.status, state.winner)
       if (data.winner !== "draw" && data.status === "resignation") {
         dispatch({ type: "SET_WINNER", player: data.winner })
         dispatch({ type: "SET_STATUS", status: data.status })
       }
     }
 
-    socket.on("connect", onConnect)
-    socket.on("disconnect", onDisconnect)
     socket.on("start", onStartGame)
     socket.on("move", onMove)
     socket.on("leave", onLeave)
     socket.on("end", onGameEnd)
 
-    if (!socket.connected) {
-      console.info("Attempting to connect...")
-      socket.connect()
-    }
-
     return () => {
-      socket.off("connect", onConnect)
-      socket.off("disconnect", onDisconnect)
       socket.off("start", onStartGame)
       socket.off("move", onMove)
       socket.off("leave", onLeave)
       socket.off("end", onGameEnd)
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [state.player.color, state.status, userId, username])
+  }, [state.player.color, state.status, state.winner])
 
-  // Effect: Timer
+  // effect: timer
   useEffect(() => {
     if (state.status !== "playing") return
 
@@ -104,12 +89,11 @@ export function useGame(userId: string, username: string) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [state.status, state.turn, state.timer])
 
-  // Effect: Sync game state between clients
+  // effect: sync game state between players
   useEffect(() => {
     const lastMove = state.previousMoves[state.previousMoves.length - 1]
     if (!lastMove) return
     if (lastMove.player === state.player.color) {
-      console.info("Sending move:", state)
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
       const { opponent, player, promotionCoordinates, ...syncState } = state
       socket.emit("move", syncState)
@@ -117,7 +101,7 @@ export function useGame(userId: string, username: string) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [state.previousMoves.length])
 
-  // Effect: Handle game end
+  // effect: end game
   useEffect(() => {
     if (isGameOver(state.status)) {
       const winner =
@@ -135,9 +119,10 @@ export function useGame(userId: string, username: string) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [state.status])
 
-  const handlePlayAgain = () => {
-    socket.emit("join", { userId, username })
+  const handlePlayAgain = async () => {
     dispatch({ type: "RESET_GAME" })
+    await sleep(1000)
+    socket.emit("join", { userId, username })
   }
 
   const handleResign = () => {
