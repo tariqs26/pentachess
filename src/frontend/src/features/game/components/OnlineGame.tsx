be252bdebc79e7ec50dcbe026c2d6674336b3a9a
KarlSader1 <saderk@mcmaster.ca>
2025-03-30 16:44:06 -0400

    feat: online game chance

--- src/frontend/src/features/game/components/OnlineGame.tsx
+++ src/frontend/src/features/game/components/OnlineGame.tsx
@@ -34,12 +34,13 @@ export const OnlineGame = ({ id: userId, username }: GameProps) => {
       ) : (
         <div className="flex w-full justify-center gap-x-2">
           <div className="relative w-full max-w-[600px]">
-            {state.promotionCoordinates && (
-              <PawnPromotionModal
-                turn={state.turn}
-                handlePromotion={handlePromotion}
-              />
-            )}
+            {state.promotionCoordinates &&
+              state.promotionCoordinates.piece.canPromote && (
+                <PawnPromotionModal
+                  turn={state.turn}
+                  handlePromotion={handlePromotion}
+                />
+              )}
             <CapturedPieces pieces={state.capturedPieces[state.player.color]} />
             <div className="relative">
               <Timer
