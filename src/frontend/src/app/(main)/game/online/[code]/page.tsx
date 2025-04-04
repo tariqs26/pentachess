986081e67cdf5a331ef55f14ce44a9872c7d67e5
Saad Tariq <saadtariq.dev@outlook.com>
2025-04-04 01:28:37 -0400

    refactor: move custom online game page into online directory

--- src/frontend/src/app/(main)/game/online/[code]/page.tsx
+++ src/frontend/src/app/(main)/game/online/[code]/page.tsx
@@ -1,8 +1,7 @@
-// TODO: Implement OnlineGamePage
-
-export default function OnlineGamePage({
+export default function CustomOnlineGamePage({
   params,
 }: Readonly<{ params: { code: string } }>) {
+  // TODO: Implement custom online game page
   return (
     <div className="mx-auto grid min-h-screen max-w-5xl place-items-center p-6">
       Game code: {params.code}
